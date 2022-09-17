05303f0809370a4866c8563ece044df77cb903e2
andreacons <consentiandrea@live.it>
2022-09-17 09:31:35 +0200

    Add project files.

---
+++ Kata11_Sorting/Program.cs
@@ -0,0 +1,4 @@
+﻿using Kata11_Sorting_Part2.Function;
+
+Console.Write("Write a phrase: ");
+StringUtilities.MySort(Console.ReadLine());
---
+++ Kata11_Sorting/Utilities/StringUtilities.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata11_Sorting_Part2.Function
+{
+    static class StringUtilities
+    {
+        public static void MySort(string inputString)
+        {
+            string cleanedString = Clean(inputString);
+            List<char> chars = cleanedString.ToList();
+
+            //applied bubble sort algorithm
+            for (int j = 0; j <= cleanedString.Length - 2; j++)
+            {
+                for (int i = 0; i <= cleanedString.Length - 2; i++)
+                {
+                    if (chars[i] > chars[i + 1])
+                    {
+                        char temp = chars[i + 1];
+                        chars[i + 1] = chars[i];
+                        chars[i] = temp;
+                    }
+                }
+            }
+
+            Console.Write("Sorted string: ");
+            foreach (char c in chars)
+            { Console.Write(c); }
+        }
+
+        public static string Clean(string stringToClean)
+        {
+            stringToClean = stringToClean.ToLower();
+            stringToClean = stringToClean.Replace(" ", "");
+            stringToClean = stringToClean.Replace(".","");
+            stringToClean = stringToClean.Replace(",", "");
+            stringToClean = stringToClean.Replace("!", "");
+            stringToClean = stringToClean.Replace(",", "");
+            stringToClean = stringToClean.Replace(",", "");
+            stringToClean = stringToClean.Replace("(", "");
+            stringToClean = stringToClean.Replace(")", "");
+            stringToClean = stringToClean.Replace("?", "");
+            stringToClean = stringToClean.Replace("'", "");
+
+            string cleanedString = stringToClean;
+            return cleanedString;
+        }
+            
+    }
+}
